9f98c81f7af8806b46c234c1272e74252ac39a86
unknown <pgidaszewski@gmail.com>
2017-06-03 15:08:46 +0200

    Added progress bar on login screen

--- app/src/main/java/com/cargocrew/cargoapp/activities/LoginActivity.java
+++ app/src/main/java/com/cargocrew/cargoapp/activities/LoginActivity.java
@@ -8,10 +8,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.cargocrew.cargoapp.R;
@@ -27,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText inputEmail, inputPassword;
     private FirebaseAuth auth;
     private Button btnSignup, btnLogin, btnReset;
+    private ProgressBar progressBarLogin;
+    private LinearLayout greyLayout;
 
 
     @Override
@@ -50,6 +55,8 @@ public class LoginActivity extends AppCompatActivity {
         btnSignup = (Button) findViewById(R.id.btn_signup);
         btnLogin = (Button) findViewById(R.id.btn_login);
         btnReset = (Button) findViewById(R.id.btn_reset_password);
+        progressBarLogin = (ProgressBar) findViewById(R.id.progressBarLogin);
+        greyLayout = (LinearLayout) findViewById(R.id.greyLayout);
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
@@ -101,6 +108,8 @@ public class LoginActivity extends AppCompatActivity {
                                         Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
                                     }
                                 } else {
+                                    greyLayout.setVisibility(View.VISIBLE);
+                                    progressBarLogin.setVisibility(View.VISIBLE);
                                     Intent intent = new Intent(LoginActivity.this, MapsActivity.class);
                                     startActivity(intent);
                                     finish();
